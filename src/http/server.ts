c4d2dca5151d3ad5ba0bb276b9f003123e5de651
Lucas Cid <lucasfelipaaa@gmail.com>
2025-05-12 23:05:58 -0300

    first commit with initials confi

---
+++ src/http/server.ts
@@ -0,0 +1,34 @@
+import "dotenv/config"
+import { fastify } from "fastify"
+import { fastifyCors } from "@fastify/cors";
+import { jsonSchemaTransform, serializerCompiler, validatorCompiler, ZodTypeProvider } from "fastify-type-provider-zod";
+import { fastifySwagger } from "@fastify/swagger";
+import fp from "fastify-plugin";
+import { fastifySwaggerUi } from "@fastify/swagger-ui";
+import { userRoutes } from "../routes/user";
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+const port = Number(process.env["PORT"]) || 5000
+app.setSerializerCompiler(serializerCompiler);
+app.setValidatorCompiler(validatorCompiler);
+
+app.register(fastifyCors, { origin : "*" });
+
+app.register(fastifySwagger, { 
+    transform : jsonSchemaTransform,
+    openapi : {
+    info : {
+        version : "1.0.0",
+        title : "Keep.me-api"
+    },
+}});
+app.register(fastifySwaggerUi, { routePrefix : "/docs", });
+const appRoutes = () => {
+    app.get("/test", (req, response) => {
+        return { message : "hello world", status : 200 }
+    })
+}
+
+app.register(appRoutes);
+app.register(userRoutes);
+app.listen({ port }).then(() => console.log(`the server is running on port ${port}`)) 
---
+++ src/routes/user.ts
@@ -0,0 +1,62 @@
+import { FastifyInstance } from "fastify";
+import { FastifyInstanceZod } from "../@types/FastifyInstanceZod";
+import { z } from "zod";
+import { randomUUID } from "node:crypto";
+interface User {
+    name : string;
+    email : string;
+    id : string;
+}
+const users : User[] = [
+    {
+        email : "jhonDoe@example.com",
+        id : "123213",
+        name : "Jon doe"
+    }
+];
+export async function userRoutes (app : FastifyInstanceZod) {
+    app.get("/user", { schema : {
+        tags : ["users"],
+        description : "Returns all users ",
+        response : {
+            200 : z.array(z.object({
+                id : z.string(),
+                name : z.string(),
+                email : z.string()
+            }))
+        }
+    }}, async (request, reply) => {
+        return reply.status(200).send(users);
+    });
+
+    app.post(
+        "/user", { 
+            schema : {
+                tags : ["users"],
+                description : "Creates a new user",
+                body : z.object({
+                    name : z.string().min(4),
+                    email : z.string().email()
+                }),
+                response : {
+                    201 : z.object({
+                        name : z.string().min(4),
+                        email : z.string().email(),
+                        id : z.string()
+                    })
+                }
+            }
+        },
+        async (request, reply) => {
+            const { body : { email, name } } = request;
+
+            const newUser : User = {
+                name, 
+                email, 
+                id : randomUUID()
+            }
+            users.push(newUser);
+            return reply.status(201).send(newUser);
+        }
+    );
+}
